971d3c2b3718c03f291a558a6f80a5166ae40462
Colby Rabideau <crabideau5691@gmail.com>
2012-02-01 00:38:09 -0500

    added some javascript dependancies

---
+++ public/templates/templates.js
@@ -0,0 +1,25 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['index'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var buffer = "", stack1, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
+
+
+  buffer += "<h1>";
+  stack1 = helpers.title || depth0.title;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "title", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "</h1>";
+  return buffer;});
+templates['test'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var buffer = "", stack1, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
+
+
+  buffer += "<p>";
+  stack1 = helpers.stuff || depth0.stuff;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "stuff", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "</p>";
+  return buffer;});
+})()
