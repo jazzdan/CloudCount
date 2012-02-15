33a8434128239a622a9ec3aff0be8bca0a007b4b
Colby Rabideau <crabideau5691@gmail.com>
2012-02-15 02:05:43 -0500

    added require js and dependency structure

---
+++ public/javascripts/modules/main.js
@@ -0,0 +1,7 @@
+require(["order!jquery", "order!backbone/underscore", "order!backbone/backbone", "backbone/json2", "backbone/handlebars"], function($) {
+    
+    $(function() {
+        alert('dependencies loaded!');
+    });
+    
+});
