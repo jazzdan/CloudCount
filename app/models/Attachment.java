1850bf6aecbaac331a1ab12780103a2f5e12e84f
Dan Miller <dan@danielsmiller.com>
2012-02-16 20:31:22 -0500

    Added attachment model that relates to user and budget.

---
+++ app/models/Attachment.java
@@ -0,0 +1,49 @@
+package models;
+ 
+import play.data.validation.Email;
+import play.data.validation.Required;
+import play.modules.morphia.Model;
+import play.modules.morphia.Model.AutoTimestamp;
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Reference;
+
+@AutoTimestamp
+@Entity
+public class Attachment extends Model {
+
+  // @Required
+  // public int id;
+
+  @Required
+  public String label;
+
+  @Required
+  public String description;
+
+  @Required
+  @Reference
+  public User uploaded_by;
+
+  @Required
+  @Reference
+  public Budget budget;
+
+  @Required
+  public int jr_node;
+
+  public Attachment(String label, String descrption, User uploaded_by, Budget budget, int jr_node) {
+    // this.id = id;
+    this.label = label;
+    this.description = description;
+    this.uploaded_by = uploaded_by;
+    this.budget = budget;
+    this.jr_node = jr_node;
+  }
+
+
+  public String toString() {
+    return label;
+  }
+
+}
