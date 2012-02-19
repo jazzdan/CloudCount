17348765e961032548f4652429e407a09ff37605
Colby Rabideau <crabideau5691@gmail.com>
2012-02-19 13:02:07 -0500

    turns out I broke the ApplicationTest.java, alteredt it to accept 302 redirect to login page

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -9,9 +9,9 @@ public class ApplicationTest extends FunctionalTest {
     @Test
     public void testThatIndexPageWorks() {
         Response response = GET("/");
-        assertIsOk(response);
-        assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
+        assertStatus(302, response);
+        // assertContentType("text/html", response);
+        // assertCharset(play.Play.defaultWebEncoding, response);
     }
     
 }
