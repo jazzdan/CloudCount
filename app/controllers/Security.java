33a2b27e9cade14e6662b9f02b53f04765f26aef
Colby Rabideau <crabideau5691@gmail.com>
2012-02-19 14:41:08 -0500

    IsAdmin?
    Added isAdmin function to controllers/Security

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -9,9 +9,21 @@ import models.*;
 
 public class Security extends Secure.Security {
     
+    /**
+     * Authenticate User
+     */
     static boolean authenticate(String email, String password) {
         User user = User.find("byEmail", email).first();
         Boolean authed = user != null && user.password.equals(password);
         return authed;
     }
+
+    /**
+     * Is admin?
+     */
+    static boolean isAdmin() {
+        User user = connected();
+        return user.admin;
+    }
+
 }
