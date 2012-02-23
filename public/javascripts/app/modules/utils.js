06dc614dab8b81c1a1c3305f4c5b08549a3b3010
Colby Rabideau <crabideau5691@gmail.com>
2012-02-23 17:32:20 -0500

    Added Modal to Utils
    Added somewhat dynamic, reusable modal dialog to the utils module

--- public/javascripts/app/modules/utils.js
+++ public/javascripts/app/modules/utils.js
@@ -16,15 +16,46 @@ function(cc, Backbone) {
   // Create a new module
   var Utils = cc.module();
 
+  /**
+   * Refresh Control Bar
+   */
   Utils.Views.RefreshBar = Backbone.LayoutManager.View.extend({
+
+    // view template
     template: "utils/controlbar-refresh",
 
+    // wrapper tag
     tagName: "div"
   });
 
+  /**
+   * Budget Control Bar
+   */
   Utils.Views.BudgetBar = Backbone.LayoutManager.View.extend({
+
+    // view template
     template: "utils/controlbar-budget",
+
   });
+
+  /**
+   * Modal Dialog
+   */
+  Utils.Views.Modal = Backbone.LayoutManager.View.extend({
+
+    // view template
+    template: 'utils/modal',
+
+    // serialized object data
+    serialize: function () {
+      var that = this; // this/that so we can get at the view
+      return {
+        title: that.options.title || 'Modal',
+        action: that.options.action || 'Ok'
+      };
+    }
+
+  })
 
   // Required, return the module for AMD compliance
   return Utils;
