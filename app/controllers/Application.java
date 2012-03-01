8c9494ab47878b3d9342f93362d2a8e91dcd13db
Dan Miller <dan@danielsmiller.com>
2012-03-01 05:52:02 -0500

    Added javadocs.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,11 @@ import models.*;
 
 import java.io.File;
 
+/**
+ * The application controller has but one method where it finds the
+ * currently connected user and renders that users view in the browser.
+ * All other application logic is handled by JavaScript
+ */
 @With(Secure.class)
 public class Application extends Controller {
 
--- app/controllers/Attachments.java
+++ app/controllers/Attachments.java
@@ -19,9 +19,21 @@ import models.User;
 import models.Node;
 import models.Attachment;
 
+
+/**
+ * The attachments controller class handles the uploading and retrieval of
+ * attachments.
+ */
 @With(Secure.class)
 public class Attachments extends Controller {
 
+  /**
+   * Takes a budgetId and returns all budgets associated with that
+   * budget. It then renders the JSON representation of that object to
+   * the view.
+   *
+   * @param  budgetId The id of the requested budget
+   */
   public static void index(long budgetId) {
     List<Attachment> attachments = Attachment.q().filter("budgetId", budgetId).asList();
     renderJSON(attachments);
@@ -31,6 +43,16 @@ public class Attachments extends Controller {
     render(budgetId);
   }
 
+  /**
+   * Creates an attachment associated with a budget and a jackrabbit
+   * node.
+   *
+   * @param budgetId The id of the budget the attachment is associated
+   * with
+   * @param label The desired label of the jackrabbit node
+   * @param description The descripton of this particular attachment
+   * @param attachment A binary file uploaded by the user
+   */
   public static void create(long budgetId, String label, String description, File attachment){
 
     // find the user
@@ -50,13 +72,26 @@ public class Attachments extends Controller {
     // render the thank you form
     render(label);
   }
-
+  /**
+   * Renders an existing attachment to the screen so that it can be
+   * downloaded by the user.
+   *
+   * @param attachmentId The id of the requested attachment
+   */
   public static void showFile(long attachmentId){
     Attachment a = Attachment.findById(attachmentId);
     JcrFile j = a.getFile();
     response.setContentTypeIfNotSet(j.getMimeType());
     renderBinary(j.getDataProvider().getInputStream());
   }
+
+  /**
+   * Deletes the requested attachment associated with the budgetId
+   *
+   * @param budgetId budgetId of the associated budget. This is
+   * necessary for constructing the URL in javascript
+   * @param attachmentId id of the attachment to be deleted
+   */
 
   public static void delete(long budgetId, long attachmentId) {
     Attachment a = Attachment.find("by_id", attachmentId).first();
