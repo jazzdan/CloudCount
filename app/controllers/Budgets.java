e21221a320cd1247dc3c455c884e1f6b737c8dc2
Dan Miller <dan@danielsmiller.com>
2012-03-01 05:54:40 -0500

    More javadocs.

--- app/controllers/Budgets.java
+++ app/controllers/Budgets.java
@@ -8,8 +8,16 @@ import java.util.*;
 import models.*;
 
 @With(Secure.class)
+
+/**
+ * Budgets controller
+ */
 public class Budgets extends Controller {
 
+    /**
+     * Constructs a list of all budgets and renders it to the view in
+     * JSON format.
+     */
     public static void index() {
       List<Budget> budgets = Budget.findAll();
       renderJSON(budgets);
