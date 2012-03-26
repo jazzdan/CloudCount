4565291a69d610b054716046bfb0392e2adf4f8f
Colby Rabideau <crabideau5691@gmail.com>
2012-03-26 17:07:48 -0400

    added listeners for keyboard events

--- public/javascripts/plugins/backbone.keyboard.js
+++ public/javascripts/plugins/backbone.keyboard.js
@@ -1,5 +1,5 @@
 /*!
- * backbone.keyboard.js v0.2.1
+ * backbone.keyboard.js v0.1.0
  * Copyright 2012, Colby Rabideau (@colbyrabideau)
  * backbone.keyboard.js may be freely distributed under the MIT license.
  */
@@ -18,14 +18,31 @@ define([
     // define Keyboard
     var Keyboard = Backbone.View.extend({
 
+        initialize: function (options) {
+
+            this.el = $(window);
+
+            this.el.bind('keyup', function () {
+                console.log('window.keyup - ' + event.keyCode);
+            });
+
+            this.el.bind('keydown', function (event) {
+                console.log('window.keydown - ' + event.keyCode);
+            });
+        },
+
         clean: function () {
-            alert('clean keyboard events');
+            window.alert('clean keyboard events');
+        },
+
+        render: function () {
+            throw "Global Keyboard view cannot be rendered.";
         }
 
     });
 
     // attach the global keyboard, and View to Backbone
     Backbone.KeyboardView = Keyboard;
-    Backbone.Keyboard = new Keyboard();
+    Backbone.Keyboard = new Keyboard({});
 
 });
