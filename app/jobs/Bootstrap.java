ff53d256b2a6761969d9949a56a86cc72f48e3e9
Dan Miller <dan@danielsmiller.com>
2012-04-01 16:31:05 -0400

    Added javadocs to Bootstrap class.

--- app/jobs/Bootstrap.java
+++ app/jobs/Bootstrap.java
@@ -3,8 +3,17 @@ package jobs;
 import play.jobs.*;
 import models.User;
 
+/**
+ * Any methods in the Bootstrap function are run when the application is
+ * started which really means when it is first hit with an HTTP Request.
+ */
+
 @OnApplicationStart
 public class Bootstrap extends Job {
+
+  /**
+   * Creates a default user and sets a default password.
+   */
   public void doJob() {
     if(User.find("username", "derp").first() == null) {
       User u = new User("derp", "Derpington", "Derpmeister", "derp@derp.com", true);
