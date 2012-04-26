ec751201e25660f0bb31f7d178e347add777c611
Carley Keefe <Carley.Keefe@gmail.com>
2012-04-26 01:24:11 -0400

    Transaction Test

---
+++ test/TransactionTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import java.util.*;
+import play.test.*;
+import models.User;
+import models.Transaction;
+import models.Budget;
+
+import play.modules.morphia.Blob;
+import play.modules.morphia.MorphiaPlugin;
+
+public class TransactionTest extends UnitTest {
+
+    @Test
+    public void aVeryImportantThingToTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testStoreTransaction() {
+	  Budget b = new Budget("derp", "derping around town", 0, 100, "derp");
+	  User u = new User();
+      Transaction t = new Transaction(b, u, 0, "derp", 0, 0, 1);
+      t.save();
+
+      t.delete();
+	  b.delete();
+	  u.delete();
+    }
+  }
